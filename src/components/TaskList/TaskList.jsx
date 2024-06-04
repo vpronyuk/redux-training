04dca4a2919de2328655d9aaf95e3c53f8ef7d5f
Volodymyr Proniuk <vpronyuk@gmail.com>
2024-06-04 22:02:21 +0300

    start code added

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,16 @@
+import { Task } from "../Task/Task";
+import css from "./TaskList.module.css";
+
+export const TaskList = () => {
+  const tasks = [];
+
+  return (
+    <ul className={css.list}>
+      {tasks.map((task) => (
+        <li className={css.listItem} key={task.id}>
+          <Task task={task} />
+        </li>
+      ))}
+    </ul>
+  );
+};
