4d9be91c8f6fdab87935f7b596a0bfd82bc24aac
Volodymyr Proniuk <vpronyuk@gmail.com>
2024-06-04 22:44:54 +0300

    added selectors

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -1,12 +1,16 @@
+import { useSelector } from "react-redux";
 import { Button } from "../Button/Button";
+import { statusFilters } from "../../redux/constants";
 import css from "./StatusFilter.module.css";
 
 export const StatusFilter = () => {
+  const filter = useSelector((state) => state.filters.status);
+
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      <Button selected={filter === statusFilters.all}>All</Button>
+      <Button selected={filter === statusFilters.active}>Active</Button>
+      <Button selected={filter === statusFilters.completed}>Completed</Button>
     </div>
   );
 };
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,12 +1,33 @@
+import { useSelector } from "react-redux";
 import { Task } from "../Task/Task";
+import { statusFilters } from "../../redux/constants";
 import css from "./TaskList.module.css";
 
+const getVisibleTasks = (tasks, statusFilter) => {
+  switch (statusFilter) {
+    case statusFilters.active:
+      return tasks.filter((task) => !task.completed);
+    case statusFilters.completed:
+      return tasks.filter((task) => task.completed);
+    default:
+      return tasks;
+  }
+};
+
 export const TaskList = () => {
-  const tasks = [];
+  // Отримуємо масив завдань із стану Redux
+  const tasks = useSelector((state) => state.tasks);
+
+  // Отримуємо значення фільтра із стану Redux
+  const statusFilter = useSelector((state) => state.filters.status);
+
+  // Обчислюємо масив завдань,
+  // які необхідно відображати в інтерфейсі
+  const visibleTasks = getVisibleTasks(tasks, statusFilter);
 
   return (
     <ul className={css.list}>
-      {tasks.map((task) => (
+      {visibleTasks.map((task) => (
         <li className={css.listItem} key={task.id}>
           <Task task={task} />
         </li>
